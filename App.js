0becb3006fd27becad78d0e192203e947d431111
BrunoLucas21 <brunolucasafreire@gmail.com>
2023-07-22 13:51:20 -0300

    declarando variaveis para mostrar botões

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import * as ImagePicker from 'expo-image-picker';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
   const [selectedImage, setSelectedImage] = useState(null);
 
   const pickImageAsync = async () => {
@@ -18,6 +20,7 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
@@ -29,12 +32,16 @@ export default function App() {
         <ImageViewer 
           placeholderImageSource={PlaceholderImage} 
           selectedImage={selectedImage}
-        />
+          />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+          {showAppOptions ? (
+            <View />
+          ) : (
+            <View style={styles.footerContainer}>
+              <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+              <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+            </View>
+          )}
       <StatusBar style='auto' />
     </View>
   );
